e5ae3d95fcdd8d766d13877554183281670586cd
Nikita Horb <masternik007@gmail.com>
2020-03-19 10:02:21 +0200

    Second vebinar

--- src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
@@ -1,25 +1,28 @@
 package ua.lviv.iot.spring.first.rest.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import ua.lviv.iot.spring.first.rest.model.Student;
-
 
 @RequestMapping("/students")
 @RestController
 public class StudentsController {
 
   @GetMapping(path = "/{id}")
-  public Student getStudent(@PathVariable("id")Integer studentID) {
+  public Student getStudent(@PathVariable("id") Integer studentID) {
     System.out.println(studentID);
-    return new Student("pedro","aldomovar");
+    return new Student("pedro", "aldomovar");
   }
-  @RequestMapping(method = {RequestMethod.GET,RequestMethod.PUT},path = "/smartest")
-  public Student getSmartestStudent() {
-    return new Student("adasda","dadasd");
+
+  @PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE, "application/x-yaml" })
+  public Student createStudent(@RequestBody Student student) {
+    student.setId(2002);
+    return student;
   }
 }
--- src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
@@ -1,24 +1,42 @@
 package ua.lviv.iot.spring.first.rest.model;
 
 public class Student {
+  private int id;
   private String firstName;
   private String lastName;
+
   public String getFirstName() {
     return firstName;
   }
+
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
+
   public String getLastName() {
     return lastName;
   }
+
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
   public Student(String firstName, String lastName) {
     super();
     this.firstName = firstName;
     this.lastName = lastName;
   }
-  
+
+  public Student() {
+    super();
+  }
+
 }
