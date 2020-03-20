35cd8be66ea1aa5579798d37b733a0d52129ca0a
Nikita Horb <masternik007@gmail.com>
2020-03-20 09:52:39 +0200

    Add 3 vebinar

--- src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
@@ -1,9 +1,19 @@
 package ua.lviv.iot.spring.first.rest.controller;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +24,37 @@ import ua.lviv.iot.spring.first.rest.model.Student;
 @RestController
 public class StudentsController {
 
+  private Map<Integer, Student> students = new HashMap<>();
+  private AtomicInteger idCounter = new AtomicInteger();
+
+  @GetMapping
+  public List<Student> getStudents() {
+    return new LinkedList<Student>(students.values());
+  }
+
   @GetMapping(path = "/{id}")
-  public Student getStudent(@PathVariable("id") Integer studentID) {
+  public Student getStudent(final @PathVariable("id") Integer studentID) {
     System.out.println(studentID);
-    return new Student("pedro", "aldomovar");
+    return students.get(studentID);
   }
 
   @PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE, "application/x-yaml" })
-  public Student createStudent(@RequestBody Student student) {
-    student.setId(2002);
+  public Student createStudent(final @RequestBody Student student) {
+    student.setId(idCounter.incrementAndGet());
+    students.put(student.getId(), student);
     return student;
+  }
+
+  @DeleteMapping(path = "/{id}")
+  public ResponseEntity<Student> deleteStudent(final @PathVariable("id") Integer studentID) {
+    HttpStatus status = students.remove(studentID) == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+    return ResponseEntity.status(status).build();
+  }
+  
+  @PutMapping(path = "/{id}")
+  public ResponseEntity<Student> updeteStudent(final @PathVariable("id") Integer studentID, final @RequestBody Student student){
+    student.setId(studentID);
+    HttpStatus status = students.put(student.getId(), student) == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+    return ResponseEntity.status(status).build();
   }
 }
