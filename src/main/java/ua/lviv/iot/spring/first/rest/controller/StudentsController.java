cc7fefea85d64e4e5a643c4fc1dcbbcb8ca90a9c
Nikita Horb <masternik007@gmail.com>
2020-03-26 10:15:25 +0200

    3 vebinar(db)

--- src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ua.lviv.iot.spring.first.business.StudentService;
 import ua.lviv.iot.spring.first.rest.model.Student;
 
 @RequestMapping("/students")
@@ -25,9 +27,13 @@ import ua.lviv.iot.spring.first.rest.model.Student;
 public class StudentsController {
 
   private Map<Integer, Student> students = new HashMap<>();
+  
   private AtomicInteger idCounter = new AtomicInteger();
-
+  
+  @Autowired
+  private StudentService studentService;
   @GetMapping
+  
   public List<Student> getStudents() {
     return new LinkedList<Student>(students.values());
   }
@@ -40,6 +46,10 @@ public class StudentsController {
 
   @PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE, "application/x-yaml" })
   public Student createStudent(final @RequestBody Student student) {
+    
+
+    System.out.println(studentService.createStudent(student));
+    
     student.setId(idCounter.incrementAndGet());
     students.put(student.getId(), student);
     return student;
--- src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
@@ -1,8 +1,19 @@
 package ua.lviv.iot.spring.first.rest.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
-  private int id;
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+  
   private String firstName;
+ 
   private String lastName;
 
   public String getFirstName() {
